4499227531cdb2f9d683e4619f0a9628fdcf888e
gelila@0913 <gelilasintayehu79@gmail.com>
2026-05-01 17:16:18 +0300

    drawing the helper

--- main.py
+++ main.py
@@ -22,3 +22,43 @@ current_solver_pos = (0, 0)
 solver_stack = []
 visited_solver = [[False for _ in range(C)] for _ in range(R)]
 
+# --- Drawing Helpers ---
+def draw_line(x1, y1, x2, y2):
+    glBegin(GL_LINES)
+    glVertex2f(x1, y1)
+    glVertex2f(x2, y2)
+    glEnd()
+
+def draw_cell_marker(r, c, color_rgb):
+    x = -0.9 + c * CELL_SIZE + (CELL_SIZE / 2.0)
+    y = 0.9 - r * CELL_SIZE - (CELL_SIZE / 2.0)
+
+    glColor3f(*color_rgb)
+    glPointSize(8.0)
+    glBegin(GL_POINTS)
+    glVertex2f(x, y)
+    glEnd()
+
+def draw_maze():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glColor3f(1.0, 1.0, 1.0)
+    glLineWidth(2.0)
+
+    for i in range(R):
+        for j in range(C):
+            x = -0.9 + j * CELL_SIZE
+            y = 0.9 - i * CELL_SIZE
+
+            x1, y1 = x, y
+            x2, y2 = x + CELL_SIZE, y - CELL_SIZE
+
+            if north_wall[i][j] == 1:
+                draw_line(x1, y1, x2, y1)
+            if east_wall[i][j] == 1:
+                draw_line(x2, y1, x2, y2)
+            if i == R - 1:
+                draw_line(x1, y2, x2, y2)
+            if j == 0:
+                draw_line(x1, y1, x1, y2)
+
+   
